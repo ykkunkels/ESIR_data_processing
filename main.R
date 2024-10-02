68edf6fdae2629388a3da084b5c0b6c5e9e8aa90
ykkunkels <ykkunkels@gmail.com>
2024-10-02 14:48:47 +0200

    v0.0.0; empty commit

---
+++ main.R
@@ -0,0 +1,27 @@
+
+######################################
+### ESIR_data_processing           ###
+### YKK, LH, BK, MP                ###
+### 02/10/2024                     ###
+### ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~###
+
+
+## 1. Load and / or Install required packages----
+# if (!require("htmltools")) {
+#   install.packages("htmltools", dep = TRUE)
+# }
+
+
+
+
+#! n. Add component: Processing raw output from RedCap---- 
+
+
+#! n. Add component: Seperating and sanatizing author names & email addresses----
+
+
+#! n. Add component: Adding tags----
+
+
+
+
